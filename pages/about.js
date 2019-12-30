3a06dbef30a65b7bb6f1a686c96fb17414b05aaa
ringo2455 <iby2455@gmail.com>
2019-12-30 17:59:10 +0900

    first commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import MyLayout from '../components/MyLayout';
+
+const About = () => {
+  return <MyLayout>Hello about next.js</MyLayout>;
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import MyLayout from '../components/MyLayout';
+
+const Index = () => {
+  return <MyLayout>Hello Next.js</MyLayout>;
+};
+
+export default Index;
